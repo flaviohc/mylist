4962158f5f82f777e672da273e1d54eba5a1e440
flaviohc <flaviocaetanno@gmail.com>
2024-04-21 15:17:43 -0300

    Sistema de importação finalizado

--- app.js
+++ app.js
@@ -1,16 +1,25 @@
 //Variáveis
 let entrada = dqs('#ent');
 let lista = [];
-let titulo = "MyList";
-let hid = document.querySelector("textarea");
+let titulo = "*_MyList_*";
+let hid = document.querySelector("#textexp");
 let concluidos = undefined;
 let textoConcluidos = "==================";
 let textoImportado = "";
 
+window.onload = function() {
+    entrada.focus();
+}
+
 //captura o texto digitado
 entrada.addEventListener('keypress', (e)=>{
     if(e.key==="Enter"){
-        listar(entrada.value);
+        if((entrada.value.search('\n\n')>-1) || (entrada.value.search('1-')>-1) || (entrada.value.search('1.')>-1) || (entrada.value.search('-\s')>-1) || entrada.value.search('\n')>-1){
+            importar();
+        }else{
+            listar(entrada.value);
+        }
+
         limparInput();
         mostrar();
     }
@@ -29,12 +38,14 @@ function limparInput(){
 
 //mostra itens da matriz Lista para o usuario
 function mostrar(){
+    let n = 1;
     dqs("#mylist").innerHTML="";
     concluidos = lista.find((element)=> element[1]==false);
 
     lista.forEach((item, index)=>{
         if(item[1]==true){
-            dqs("#mylist").insertAdjacentHTML('beforeend','<div class="item" id="'+index+'"><span class="hidden"><input type="checkbox" id="cb'+index+'"></span>'+item[0]+'</div>');
+            dqs("#mylist").insertAdjacentHTML('beforeend','<div class="item" id="'+index+'">'+n+"- "+item[0]+'</div>');
+            n++;
         }
     })
     if(concluidos != undefined){
@@ -73,12 +84,14 @@ function exportar(){
 }
 
 function criarTextoExportacao(){
+    let n = 1;
     let text = "";
     let listaExcluidos = false;
-    text += "*_"+titulo+"_* &#10;"
+    text += titulo+"&#10;"
     lista.forEach((item)=>{
         if(item[1]==true){
-            text += "&#10;- "+ item[0];
+            text += "&#10;"+n+". "+ item[0];
+            n++;
         }
         if(item[1]==false){
             listaExcluidos = true;
@@ -96,48 +109,63 @@ function criarTextoExportacao(){
     return text;
 }
 
-dqs("#imp").addEventListener('click',()=>{
-    importar();
-})
-
 //importar lista
 function importar(){
     //encontrar pontos com duas quebras de linha
-    textoImportado = dqs("#textimp").value;
+    textoImportado = entrada.value;
+    let arListTrue = [];
     const searchStr = '\n\n';
     const indexes = [...textoImportado.matchAll(new RegExp(searchStr, 'gi'))].map(a => a.index);
     //console.log(indexes); // [2, 25, 27, 33]
     let arTodo=textoImportado.split('\n\n');
-    let arListTrue = arTodo[1].split('\n');
-    // console.log(arList);
 
+    //TITULO
+    if(arTodo.length>1){
+        dqs("#titulo").innerHTML=arTodo[0];
+        if(arTodo[0].search('\n')>-1){
+            let primeiraQuebraTitulo = arTodo[0].search('\n');
+            arTodo[0]="*"+arTodo[0].slice(0,primeiraQuebraTitulo)+"*"+arTodo[0].slice(primeiraQuebraTitulo);
+        }
+        titulo = arTodo[0];
+        arListTrue = arTodo[1].split('\n');
+    }
+
+    // console.log(arTodo.lenght);
+    //LISTA PRINCIPAL
+    if(arTodo.lenght==undefined){
+        arListTrue = textoImportado.split('\n');
+    }
     arListTrue.forEach((item)=>{
+        let it = "";
         let i = item.match(/\-|\./);
-        let it = item.slice(i["index"]+1);
+        if(i!=undefined){
+            it = item.slice(i["index"]+1);
+        }else{
+            it= item;
+        }
         it = it.trimStart().trimEnd();
         if(it.trim()!=""){
             lista.push([it,true]);
         }
     })
-    // console.log(lista);
-    dqs("#titulo").innerHTML=arTodo[0];
 
-    let arListFalse = arTodo[2].split('\n');
-    arListFalse.shift();
-    arListFalse.forEach((item)=>{
-        let i = item.match(/\-|\./);
-        let it = item.slice(i["index"]+1);
-        it = it.trimStart().trimEnd();
-        if(it.search("~")>-1){
-            it.substring(1);
+    //LISTA EXCLUIDOS
+    if(arTodo.length>2){
+        let arListFalse = arTodo[2].split('\n');
+        arListFalse.shift();
+        arListFalse.forEach((item)=>{
+            let i = item.match(/\-|\./);
+            let it = item.slice(i["index"]+1);
+            it = it.trimStart().trimEnd();
             if(it.search("~")>-1){
-                it.slice(0,-1);
+                it.substring(1);
+                if(it.search("~")>-1){
+                    it.slice(0,-1);
+                }
             }
-        }
-        if(it.trim()!=""){
-            lista.push([it,false]);
-        }
-    })
-
-    mostrar();
+            if(it.trim()!=""){
+                lista.push([it,false]);
+            }
+        })
+    }
 }
